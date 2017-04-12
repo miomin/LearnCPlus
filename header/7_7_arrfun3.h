c94ae51774650f104c8e08138cf8b015e1519b78
miomin <691292118@qq.com>
2017-04-12 18:11:08 +0800

    add

---
+++ header/7_7_arrfun3.h
@@ -0,0 +1,18 @@
+//
+// Created by 莫绪旻 on 17/2/24.
+//
+
+#ifndef LEARNCPLUS_7_7_ARRFUN3_H
+#define LEARNCPLUS_7_7_ARRFUN3_H
+
+const int MAX = 5;
+
+void arrfun3();
+
+int fill_array(double ar[], int limit);
+
+void show_array(const double ar[], int length);
+
+bool revalue(double r, double ar[], int length, int index);
+
+#endif //LEARNCPLUS_7_7_ARRFUN3_H
